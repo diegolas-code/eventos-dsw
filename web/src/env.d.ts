e64a8256582fdf6afe80c4aafce9b071217f1ed6
Diegolas <diegolas.code@gmail.com>
2026-05-17 00:18:21 -0300

    chore(web): add env.d.ts to allow CSS/static imports in TS

---
+++ web/src/env.d.ts
@@ -0,0 +1,9 @@
+// Type declarations for importing static assets in the frontend
+declare module '*.css';
+declare module '*.scss';
+declare module '*.png';
+declare module '*.jpg';
+declare module '*.jpeg';
+declare module '*.svg';
+
+export {};
