35b2588e68012958422ba61acc72b0ff3db5089c
Diegolas <diegolas.code@gmail.com>
2026-06-16 19:36:24 -0300

    feat(frontend): add request interceptor to attach jwt token to Axios

--- web/src/services/api.ts
+++ web/src/services/api.ts
@@ -1,5 +1,13 @@
-import axios from "axios";
+import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: "http://localhost:3001/api/v1",
+  baseURL: 'http://localhost:3001/api/v1',
+});
+
+api.interceptors.request.use(config => {
+  const token = localStorage.getItem('token');
+  if (token && config.headers) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
 });
